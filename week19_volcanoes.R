3362e8736351b8bfe097bc3327afeae999fc95e1
Vanessa Burton <vanessa.burton@ed.ac.uk>
2020-05-12 17:10:11 +0100

    start adding map and focusing on NZ

--- week19_volcanoes.R
+++ week19_volcanoes.R
@@ -3,6 +3,13 @@ library(readxl)
 library(tidyverse)
 library(ggExtra)
 library(RColorBrewer)
+library(sf)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(rgeos)
+
+# set plot theme
+theme_set(ggsidekick::theme_sleek())
 
 # load data
 tuesdata <- tidytuesdayR::tt_load(2020, week = 20)
@@ -43,16 +50,38 @@ destruct <- v.data %>%
 destruct %>% ggplot()+
   geom_jitter(aes(last_eruption_year,vei,size=vei,color=vei))+
   #scale_fill_gradient2(low='yellow',mid='orange',high='red',aesthetics='fill')+
-  ggsidekick::theme_sleek()+
+  #ggsidekick::theme_sleek()+
   xlab("Date of last eruption")+
   ylab("VEI (Explosivity Index)")+
   theme(legend.position='none')+
   theme(panel.background = element_rect(fill='black'))
-  
+
+# load world map
+world <- ne_countries(scale = "medium", returnclass = "sf")
+class(world)
+ggplot(data = world) +
+  geom_sf()
+ggplot(data = world) +
+  geom_sf(colour='grey',fill='#008080') +
+  xlab("Longitude") + ylab("Latitude") +
+  ggtitle("World map", subtitle = paste0("(", length(unique(world$name)), " countries)"))
+
+library("ggspatial")
+world %>% filter(name=="New Zealand") %>% 
+  ggplot() +
+  geom_sf(colour='grey',fill='#008080') +
+  xlab("Longitude") + ylab("Latitude")+
+  annotation_scale(location = "bl", width_hint = 0.5) +
+  annotation_north_arrow(location = "bl", which_north = "true", 
+                         pad_x = unit(0.75, "in"), pad_y = unit(0.5, "in"),
+                         style = north_arrow_fancy_orienteering) +
+  coord_sf(xlim = c(166, 180), ylim = c(-55,-30))
+
 # largest NZ eruptions
 unique(v.data$subregion)
 NZ <- v.data %>% 
   filter(subregion=="New Zealand") %>% 
   ggplot()+
   geom_jitter(aes(x=longitude.x,y=latitude.x,size=vei))
-NZ
+
+NZ + map
