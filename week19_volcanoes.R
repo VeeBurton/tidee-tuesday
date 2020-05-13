c095659091ed1e9c6a454d48a71f95c039c63b71
Vanessa Burton <vanessa.burton@ForestResearch.gov.uk>
2020-05-13 12:31:57 +0100

    edits using ggmap

--- week19_volcanoes.R
+++ week19_volcanoes.R
@@ -7,24 +7,25 @@ library(sf)
 library(rnaturalearth)
 library(rnaturalearthdata)
 library(rgeos)
+library(ggmap)
 
 # set plot theme
-theme_set(ggsidekick::theme_sleek())
+#theme_set(ggsidekick::theme_sleek())
 
 # load data
 tuesdata <- tidytuesdayR::tt_load(2020, week = 20)
 
 # cleaning
-eruptions <- tuesdata$eruptions %>% 
-  janitor::clean_names() %>% 
-  select(-contains("modifier"), -contains("uncertainty"))
-events <- tuesdata$events %>% 
-  janitor::clean_names() %>% 
-  select(-contains("modifier"), -contains("uncertainty"))
-volcanoes <- tuesdata$volcano %>% 
-  janitor::clean_names()
-treerings <- tuesdata$tree_rings %>% 
-  janitor::clean_names()
+eruptions <- tuesdata$eruptions #%>% 
+  #janitor::clean_names() %>% 
+  #select(-contains("modifier"), -contains("uncertainty"))
+events <- tuesdata$events #%>% 
+  #janitor::clean_names() %>% 
+  #select(-contains("modifier"), -contains("uncertainty"))
+volcanoes <- tuesdata$volcano #%>% 
+  #janitor::clean_names()
+treerings <- tuesdata$tree_rings #%>% 
+  #janitor::clean_names()
 
 # explore
 summary(eruptions)
@@ -40,7 +41,7 @@ unique(events$event_type)
 
 v.data <- merge(volcanoes,eruptions,by="volcano_number")
 summary(v.data)
-v.data[!duplicated(v.data),]
+v.data<-v.data[!duplicated(v.data),]
 
 v.data$last_eruption_year <- as.numeric(v.data$last_eruption_year)
 destruct <- v.data %>% 
@@ -57,31 +58,40 @@ destruct %>% ggplot()+
   theme(panel.background = element_rect(fill='black'))
 
 # load world map
-world <- ne_countries(scale = "medium", returnclass = "sf")
-class(world)
-ggplot(data = world) +
-  geom_sf()
-ggplot(data = world) +
-  geom_sf(colour='grey',fill='#008080') +
-  xlab("Longitude") + ylab("Latitude") +
-  ggtitle("World map", subtitle = paste0("(", length(unique(world$name)), " countries)"))
+#world <- ne_countries(scale = "medium", returnclass = "sf")
+#class(world)
+#ggplot(data = world) +
+  #geom_sf()
+#ggplot(data = world) +
+  #geom_sf(colour='grey',fill='#008080') +
+  #xlab("Longitude") + ylab("Latitude") +
+  #ggtitle("World map", subtitle = paste0("(", length(unique(world$name)), " countries)"))
 
-library("ggspatial")
-world %>% filter(name=="New Zealand") %>% 
-  ggplot() +
-  geom_sf(colour='grey',fill='#008080') +
-  xlab("Longitude") + ylab("Latitude")+
-  annotation_scale(location = "bl", width_hint = 0.5) +
-  annotation_north_arrow(location = "bl", which_north = "true", 
-                         pad_x = unit(0.75, "in"), pad_y = unit(0.5, "in"),
-                         style = north_arrow_fancy_orienteering) +
-  coord_sf(xlim = c(166, 180), ylim = c(-55,-30))
+#library("ggspatial")
+#world %>% filter(name=="New Zealand") %>% 
+  #ggplot() +
+  #geom_sf(colour='grey',fill='#008080') +
+  #xlab("Longitude") + ylab("Latitude")+
+  #annotation_scale(location = "bl", width_hint = 0.5) +
+  #annotation_north_arrow(location = "bl", which_north = "true", 
+                         #pad_x = unit(0.75, "in"), pad_y = unit(0.5, "in"),
+                         #style = north_arrow_fancy_orienteering) +
+  #coord_sf(xlim = c(166, 180), ylim = c(-55,-30))
 
+# use ggmap instead
+NZ<- c(lon = 174, lat = -40)
+taupo <- c(long=176,lat=-38.6)
+NZmap <- get_map(taupo, zoom=7, scale=1, maptype = "watercolor", source = "stamen")
+ggmap(NZmap)
 # largest NZ eruptions
 unique(v.data$subregion)
-NZ <- v.data %>% 
-  filter(subregion=="New Zealand") %>% 
-  ggplot()+
-  geom_jitter(aes(x=longitude.x,y=latitude.x,size=vei))
+NZ.volc <- v.data[which(v.data$subregion=="New Zealand" & v.data$last_eruption_year>1900),]
+unique(NZ.volc$volcano_name.x)
+unique(NZ.volc$last_eruption_year)
 
-NZ + map
+ggmap(NZmap)+
+  geom_jitter(data = NZ.volc, aes(x=longitude.x,y=latitude.x,
+                                  size=vei,
+                                  color=volcano_name.x),
+              shape=2,
+              stroke=2)
